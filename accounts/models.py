e03186997f51d25b44a031993393e6a62d7906f6
Abhishek <absharma1099@gmail.com>
2020-09-15 11:40:37 +0530

    DB connected

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=70, blank=False, default='')
+    password = models.CharField(max_length=200,blank=False, default='')
+   
+
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import User
 
 # Create your views here.
 
+
 def register(request):
-    return render(request,'register.html')
+    if request.method=='POST':
+        print("I'm here")
+        username=request.POST['username']
+        password=request.POST['password1']
+        user=User(username=username, password=password)
+        user.save()
+        return redirect('/home')
+    else:
+        return render(request,'register.html')
 
 def login(request):
     return render(request,'login.html')
+
+def home(request):
+    return render(request,'home.html') 
